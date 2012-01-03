fc6b60703ed13d843a0c6f2d4f0732a18a44bf27
Sudar <sudar@sudarmuthu.com>
2012-01-03 17:00:14 +0530

    Fixed typos and added installation of vundle

--- setup-links.sh
+++ setup-links.sh
@@ -2,8 +2,10 @@
 
 ln -sv $PWD/bash/bashrc ~/.bashrc
 ln -sv $PWD/bash/bash_profile ~/.bash_profile
-ln -sv $PWD/bash/bash_alias ~/.bash_alias
+ln -sv $PWD/bash/bash_aliases ~/.bash_aliases
 
-ln -sv $PWD/bash/vimrc ~/.vimrc
-ln -sv $PWD/bash/vim ~/.vim
+ln -sv $PWD/vim/vimrc ~/.vimrc
+ln -sv $PWD/vim/vim ~/.vim
 
+# Install vundle
+git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
