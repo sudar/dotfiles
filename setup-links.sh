01f16e7832f1fc304c5f0d58d7f04c1e7e728c4b
Sudar <sudar@sudarmuthu.com>
2012-01-03 15:10:55 +0530

    Added shell script to setup links

---
+++ setup-links.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+ln -sv $PWD/bash/bashrc ~/.bashrc
+ln -sv $PWD/bash/bash_profile ~/.bash_profile
+ln -sv $PWD/bash/bash_alias ~/.bash_alias
+
+ln -sv $PWD/bash/vimrc ~/.vimrc
+ln -sv $PWD/bash/vim ~/.vim
+
