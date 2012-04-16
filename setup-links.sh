873ac80951c25115334613dd81123c02c4b45638
Sudar <sudar@sudarmuthu.com>
2012-04-16 16:16:08 +0530

    Added gitconfig file to the repo as well

--- setup-links.sh
+++ setup-links.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
+# setups proper syslins
+
+# Bash files
 ln -sv $PWD/bash/bashrc ~/.bashrc
 ln -sv $PWD/bash/bash_profile ~/.bash_profile
 ln -sv $PWD/bash/bash_aliases ~/.bash_aliases
 
+# git files
+ln -sv $PWD/git/gitconfig ~/.gitconfig
+
+# vim files
 ln -sv $PWD/vim/vimrc ~/.vimrc
 ln -sv $PWD/vim/vim ~/.vim
 
