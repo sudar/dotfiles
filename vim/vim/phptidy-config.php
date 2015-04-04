c1d03888b4a1e324ce1e76cc0b383b5f6427e59d
Sudar <sudar@sudarmuthu.com>
2015-04-04 06:48:08 +0530

    Add phptidy config file

---
+++ vim/vim/phptidy-config.php
@@ -0,0 +1,10 @@
+<?php
+$default_package = 'BulkDelete';
+$default_author = 'Sudar';
+$indent_char = "\t";
+$add_file_docblock = true;
+$fix_docblock_format = true;
+$fix_docblock_space = true;
+$add_function_docblocks = true;
+$add_blank_lines = false;
+$fix_round_bracket_space = true;
