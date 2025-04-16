15c602b37676e6f0b198a333a43635f9b31b355d
Sudar Muthu <sudar@sudarmuthu.com>
2025-04-16 23:02:07 +0530

    Add brew related alias

---
+++ zsh/custom/brew.zsh
@@ -0,0 +1,23 @@
+##############################################################################
+# These are the list of alias that I use for brew.
+# Feel free to use the entire file or parts of it.
+#
+# Author: Sudar (https://sudarmuthu.com)
+# Repo: https://github.com/sudar/dotfiles
+#
+##############################################################################
+
+# Shorter alias for brew
+alias b='brew'
+
+# Update and upgrade brew
+alias bupg='brew update && brew upgrade'
+
+# Generate the brewfile that contains all the installed packages
+alias bbg='brew bundle dump --describe --force --file=~/dev/dotefiles/brew/Brewfile'
+
+# Install the packages listed in the brewfile
+alias bbi='brew bundle --file=~/dev/dotefiles/brew/Brewfile'
+
+# List only the packages installed by brew
+alias bli='brew list --installed-on-request'
