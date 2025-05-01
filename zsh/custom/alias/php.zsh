82f1db356c5ff3ce95cd1805a85974e07671d63a
Sudar Muthu <sudar@sudarmuthu.com>
2025-05-01 09:22:54 +0530

    Add PHP specific alias

---
+++ zsh/custom/alias/php.zsh
@@ -0,0 +1,14 @@
+##############################################################################
+# PHP specific custom configuration for zsh
+# Feel free to use the entire file or parts of it.
+#
+# Author: Sudar (https://sudarmuthu.com)
+# Repo: https://github.com/sudar/dotfiles
+#
+##############################################################################
+
+#======
+# composer
+#======
+alias c="composer"
+alias ci="composer install"
